4757b6417ca72143b68a0029c5cf57d56a97bb82
Yuri Burk <yuriburk@gmail.com>
2020-05-05 11:39:40 -0300

    chore: Update service worker

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -3,3 +3,4 @@ import { wrapRootElement as wrap } from './.gatsby/wrapRootElement';
 import './src/styles/themes/dracula.css';
 
 export const wrapRootElement = wrap;
+export const onServiceWorkerUpdateReady = (): void => window.location.reload();
