e79e6589ffbf6852ac243d90ea46bc3946e66fb2
Yuri Burk <yuriburk@gmail.com>
2020-05-09 21:52:35 -0300

    chore: Created config.ts file

---
+++ data/config.ts
@@ -0,0 +1,13 @@
+const config = {
+  siteTitle: 'Yuri Burk',
+  siteDescription: 'O melhor blog de programação do Brasil :)',
+  siteTitleShort: 'Yuri Burk',
+  siteRss: '/rss.xml',
+  siteImage: '',
+  googleAnalyticsID: process.env.GATSBY_GOOGLE_ANALYTICS_ID,
+  themeColor: '#000000',
+  backgroundColor: '#1a1a1a',
+  siteUrl: 'https://yuriburk.netlify.app/',
+};
+
+export default config;
