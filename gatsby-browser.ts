0085803ee0f7d168aac101109563569decfbdfcb
Yuri Burk <yuriburk@gmail.com>
2020-05-17 00:19:58 -0300

    chore: Service worker test

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -3,4 +3,13 @@ import { wrapRootElement as wrap } from './.gatsby/wrapRootElement';
 import './src/styles/themes/dracula.css';
 
 export const wrapRootElement = wrap;
-export const onServiceWorkerUpdateReady = (): void => window.location.reload();
+export const onServiceWorkerUpdateReady = (): void => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`,
+  );
+
+  if (answer) {
+    window.location.reload();
+  }
+};
