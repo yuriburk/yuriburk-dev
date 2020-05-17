800410bed438213d7d19e1729f9d331e096e027e
Yuri Burk <yuriburk@gmail.com>
2020-05-17 00:27:30 -0300

    chore: service worker update ready

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -4,12 +4,5 @@ import './src/styles/themes/dracula.css';
 
 export const wrapRootElement = wrap;
 export const onServiceWorkerUpdateReady = (): void => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`,
-  );
-
-  if (answer) {
-    window.location.reload();
-  }
+  window.location.reload();
 };
