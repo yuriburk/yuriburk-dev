575eccc5a5d6246543f525ca5de093f5949b646b
Yuri Burk <yuriburk@gmail.com>
2020-10-20 15:10:53 -0300

    Update index.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -42,9 +42,7 @@ const About: React.FC = () => (
         </p>
         <br />
         <p>
-          Minha principal área de conhecimento é desenvolvimento back-end
-          utilizando C#, mas desde o último ano comecei a estudar bastante para
-          me tornar especialista em mobile e front-end, utilizando React.
+          Minha principal área de conhecimento é desenvolvimento front-end com React, mobile com React Native e back-end utilizando C# ou Node.js.
         </p>
         <br />
         <p>
